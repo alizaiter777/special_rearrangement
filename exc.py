f35d6de5c7352f617cf26c74b57dbac3b9fb6c53
alizaiter777 <alizaiter777@gmail.com>
2024-11-04 14:40:39 +0200

    create special_rearrangement function

--- exc.py
+++ exc.py
@@ -4,11 +4,20 @@
 #using map to convert each string to integer after splitting
 nums = list(map(int, input("Enter  list of integers : ").split()))
 
-# generate 2 lists
+#generate 2 lists
 even=[]
 odd=[]
 
+#this function list user input and specify integers in 2 list even or odd
+def special_rearrangement(nums):
+   #this for loop to path all integers to if
+   for num in nums:
+      if num % 2 == 0:
+         even.append(num)
+      else:
+       odd.append(num)
 
+   return special_rearrangement(nums)
 
 
    
