51d7c9de15ed08af596b614e5253b801459a388d
alizaiter777 <alizaiter777@gmail.com>
2024-11-04 14:22:30 +0200

    generate  lists

--- exc.py
+++ exc.py
@@ -1,7 +1,12 @@
 
 
+#using split function to split user input according to space
+#using map to convert each string to integer after splitting
+nums = list(map(int, input("Enter  list of integers : ").split()))
 
-nums = list(map(int, input("Enter list of integers : ").split()))
+# generate 2 lists
+even=[]
+odd=[]
 
 
    
